824bcfc2e5b50155fa82cc54be042e23ab7a77f8
Megan <megan.chang16@gmail.com>
2026-07-12 17:23:58 +0800

    Add dark mode button

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ function showMessage() {
   const message = document.getElementById("message");
   message.textContent = "欢迎来到我的个人主页！谢谢你的访问。";
 }
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
